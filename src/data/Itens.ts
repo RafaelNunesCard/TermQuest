0116a25928c2097f341a6cf443dbf0e5c0a1f97c
RafaelNunesCard <rfnunes1000@gmail.com>
2026-05-27 10:14:49 -0300

    Adicionei mais itens para a loja

--- src/data/Itens.ts
+++ src/data/Itens.ts
@@ -1,18 +1,40 @@
 import { Item } from '../models/item';
-import { efeitos } from './Efeitos';
 
+// ===== Equipamentos =====
 const espadaQuebrada: Item = {
     nome: "Espada Quebrada",
     tipo: "Equipamento",
-    descricao: "Aumenta 10 de dano",
-    valor: 30,
+    descricao: "Aumenta 7 de dano",
+    valor: 35,
     efeito: {
-        ataque: 10
+        ataque: 7
     }
 }
 
-const pocaoMana: Item = {
-    nome: "Poção de Mana",
+const escudoQuebrado: Item = {
+    nome: "Escudo Quebrado",
+    tipo: "Equipamento",
+    descricao: "Aumenta 6 de defesa",
+    valor: 35,
+    efeito: {
+        defesa: 6
+    }
+}
+
+const armaduraFerro: Item = {
+    nome: "Armadura de Ferro",
+    tipo: "Equipamento",
+    descricao: "Aumenta 20 de defesa",
+    valor: 90,
+    efeito: {
+        defesa: 20
+    }
+}
+
+
+// ===== Poções =====
+const pocaoPMana: Item = {
+    nome: "Poção pequena de Mana",
     tipo: "Poção",
     descricao: "Recupera 15 de mana",
     valor: 15,
@@ -21,12 +43,48 @@ const pocaoMana: Item = {
     }
 }
 
+const pocaoMMana: Item = {
+    nome: "Poção média de Mana",
+    tipo: "Poção",
+    descricao: "Recupera 40 de mana",
+    valor: 35,
+    efeito: {
+        energia: 40
+    }
+}
 
+const pocaoPHp: Item = {
+    nome: "Poção pequena de Vida",
+    tipo: 'Poção',
+    descricao: 'Recupera 12 de vida',
+    valor: 20,
+    efeito: {
+        hp: 12
+    }
+}
+
+const pocaoMHp: Item = {
+    nome: "Poção média de Vida",
+    tipo: 'Poção',
+    descricao: 'Recupera 40 de vida',
+    valor: 60,
+    efeito: {
+        hp: 40
+    }
+}
+
+// ===== Chaves =====
 
 
 const Itens = [
     espadaQuebrada,
-    pocaoMana
+    escudoQuebrado,
+    armaduraFerro,
+
+    pocaoPMana,
+    pocaoMMana,
+    pocaoPHp,
+    pocaoMHp,
 ]
 
 export { Itens }
